b80bd036ad5b93ae4816b15a9bf8a333043ed8d2
lsmman <tmdgus507@naver.com>
2021-05-05 02:18:53 +0900

    add fmt_deal_with_scan_error

--- 5.fmt/fmt_deal_with_scan_error.go
+++ 5.fmt/fmt_deal_with_scan_error.go
@@ -1,19 +1,3 @@
-// /* 실행방법
-// 1. go mod init package_name (=fold_name)
-// 2. go build
-// 3. 실행 파일 ex) hello.exe
-// */
-
-// package main
-
-// import "fmt"
-
-// func main() {
-// 	var msg string = "Lim"
-
-// 	fmt.Println("Hello", msg)
-// }
-
 package main
 
 import (
